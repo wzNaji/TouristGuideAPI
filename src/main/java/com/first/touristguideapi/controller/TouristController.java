16a919e3e19d8a0faa1ae87da6c49081f5b9b215
wzNaji <141943564+wzNaji@users.noreply.github.com>
2024-02-02 23:51:29 +0100

    4. commit
    Minor changes

--- src/main/java/com/first/touristguideapi/controller/TouristController.java
+++ src/main/java/com/first/touristguideapi/controller/TouristController.java
@@ -5,13 +5,13 @@ import com.first.touristguideapi.model.TouristAttraction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("attractions")
 public class TouristController {
 
